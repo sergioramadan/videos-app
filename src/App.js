d23affa0785e4cb4aa67b2e0492b3f120b79442e
Sergio Ramadan <sergioramadan@Sergios-MacBook-Pro.local>
2020-06-22 21:00:19 -0300

    Create login page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Login from './components/Login';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +8,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <CssBaseline />
+        <Login />
+        {
+          /*
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          */
+        }
       </div>
     );
   }
---
+++ src/components/Topbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+function Topbar () {
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Topbar;
